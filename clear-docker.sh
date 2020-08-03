b4f887c865c623208a157d4bd548247093b5ef18
Henrique Flach Latorre Moreno <53087088+henriquemoreno1981@users.noreply.github.com>
2020-08-02 23:53:43 -0300

    Add files via upload
    Limpa do docker.

---
+++ clear-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#killall Docker
+rm -rf $HOME/.docker $HOME/Library/Caches/com.docker.docker $HOME/Library/Containers/com.docker.docker $HOME/Library/Group\ Containers/group.com.docker/
