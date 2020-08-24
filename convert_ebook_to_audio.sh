0d6521e3b482edbc539ce3ba70b411aae3b04201
Henrique Flach Latorre Moreno <53087088+henriquemoreno1981@users.noreply.github.com>
2020-08-24 13:27:16 -0300

    Update convert_ebook_to_audio.sh

--- convert_ebook_to_audio.sh
+++ convert_ebook_to_audio.sh
@@ -3,6 +3,7 @@ filename="$1"
 filenameOnly=$(echo "$filename" | cut -f 1 -d '.')
 textFile="${filenameOnly}.txt"
 audioFile="${filenameOnly}.aiff"
+mp3File="${filenameOnly}.mp3"
 echo convertendo $filename
 ebook-convert "$filename" "$textFile" > /dev/null
 voice="Felipe"
@@ -11,6 +12,7 @@ then
 	voice="Felipe"
 fi
 echo transformando em "${audioFile}"
-say --progress -f "${textFile}" -o "${audioFile}" -v "$voice" 
-rm "${textFile}"
+say --progress -f "${textFile}" -o "${audioFile}" -v "$voice"
+ffmpeg -i "${audioFile}" -acodec libmp3lame -ab 192000 -ar 44100 "${mp3File}"
+rm "${textFile}" "${audioFile}"
 echo "Fim."
